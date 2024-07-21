851321a7101decbaa5cf9964b1b4bfa73bbd5ddf
bill.lin <zzx123bill@gmail.com>
2024-07-21 15:54:21 +0800

    modify update.py

--- update.py
+++ update.py
@@ -8,13 +8,33 @@ import twstock
 codes = twstock.codes
 
 
+def clean_csv(csv_path):
+    """ Clean the CSV file by removing rows with incorrect number of columns. """
+    with open(csv_path, 'r') as file:
+        lines = file.readlines()
+
+    # Count the number of columns in the header
+    num_columns = len(lines[0].split(','))
+
+    with open(csv_path, 'w') as file:
+        for line in lines:
+            if len(line.split(',')) == num_columns:
+                file.write(line)
+
+
 def get_data_since_last_record(stock_num, base_path='./data/'):
     csv_path = f'{base_path}{stock_num}.csv'
     tz_taipei = pytz.timezone('Asia/Taipei')
     today = datetime.now(tz_taipei).replace(hour=0, minute=0, second=0, microsecond=0)  # Reset to start of day
 
     if os.path.exists(csv_path):
-        data = pd.read_csv(csv_path, header=0)
+        try:
+            clean_csv(csv_path)  # Clean the CSV file before reading
+            data = pd.read_csv(csv_path, header=0)
+        except pd.errors.ParserError as e:
+            print(f"Parser error when reading CSV: {e}")
+            data = pd.DataFrame()  # Fallback to an empty DataFrame
+
         if not data.empty:
             try:
                 last_record_date = pd.to_datetime(data['Datetime'].iloc[-1]).tz_convert('Asia/Taipei')
